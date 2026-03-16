1f2abe528cc2d3af8d483262a2bb3ac94e832005
Julián MR <julian.marin42@eia.edu.co>
2026-03-16 09:44:34 -0500

    Intento #1000

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename='/concerthub-fork' >
+    <HashRouter >
       <App />
     </HashRouter>
   </StrictMode>,
